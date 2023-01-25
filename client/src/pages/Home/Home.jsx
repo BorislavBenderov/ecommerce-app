9addb7b9fdb457510a0c1bc11a930eb2d5ff39e9
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-25 08:28:54 +0200

    Add Home page jsx

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,16 @@
+import { FeaturedProducts } from "../../components/FeauturedProducts/FeaturedProducts";
+import { Slider } from "../../components/Slider/Slider";
+import { Categories } from "../../components/Categories/Categories";
+import { Contact } from "../../components/Contact/Contact";
+
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <Slider />
+      <FeaturedProducts type="feautured" />
+      <Categories />
+      <FeaturedProducts type="trending" />
+      <Contact />
+    </div>
+  );
+};
